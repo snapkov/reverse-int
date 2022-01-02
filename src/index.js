098fc6daee33ede0e32fb942904f3bfffbd76f70
snapkov <snapkov@gmail.com>
2022-01-02 11:49:26 +0300

    feat: add function that reverse digits of this number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+    let absInt = Math.abs(n);
+    let arrayInt = absInt.toString().split("");
+    let arrayReverse = [];
+    let reverseInt = 0;
+
+    for (let i = 0; i < arrayInt.length; i++) {
+        arrayReverse.unshift(arrayInt[i]);
+    }
+
+    reverseInt = Number(arrayReverse.join(""));
+    return reverseInt;
 }
+
